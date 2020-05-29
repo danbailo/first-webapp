379d39c6439ca1dd1aa8ae91f8aba8ad78a4eb79
Daniel Bailo <danbailoufms@gmail.com>
2020-05-29 03:35:51 -0300

    relationship between tables

--- src/app.py
+++ src/app.py
@@ -12,9 +12,20 @@ class User(db.Model):
 	name = db.Column(db.String(32), nullable=False)
 	email = db.Column(db.String(64), unique=True, nullable=False, index=True)
 	password = db.Column(db.String(255), nullable=False)
+	profile = db.relationship('Profile', backref='user', uselist=False)
 
 	def __str__(self):
 		return f"{self.name}"
+
+class Profile(db.Model):
+	__tablename__ = "profiles"
+	id_profile = db.Column(db.Integer, primary_key=True)
+	photo = db.Column(db.Unicode(124), nullable=False)
+	id_user = db.Column(db.Integer, db.ForeignKey("users.id_user"))
+
+	def __str__(self):
+		return f"{self.name}"
+
 
 @app.route("/")
 def index():
