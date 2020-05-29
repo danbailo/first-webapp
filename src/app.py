c3844e9adde02ebe37678a472fda6a2609a1eb0e
Daniel Bailo <danbailoufms@gmail.com>
2020-05-29 01:29:09 -0300

    create database and first model

--- src/app.py
+++ src/app.py
@@ -6,6 +6,21 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
+class Users(db.Model):
+	__tablename__ = "users"
+	id_user = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(32), nullable=False)
+	email = db.Column(db.String(64), unique=True, nullable=False, index=True)
+	password = db.Column(db.String(255), nullable=False)
+
+	def __str__(self):
+		return f"{self.name}"
+
+@app.before_first_request
+def create_database():
+    db.create_all()
+
+
 @app.route("/")
 def index():
 	return "<a href='/posts'>Posts</a>"
