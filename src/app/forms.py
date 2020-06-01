b484970ccd584505a6a3395b12bdeb8e10ab00a6
Daniel Bailo <danbailoufms@gmail.com>
2020-05-31 23:30:55 -0300

    added compatibility with button in render_template

--- src/app/forms.py
+++ src/app/forms.py
@@ -1,7 +1,38 @@
 from flask_wtf import FlaskForm
-from wtforms.fields import BooleanField, PasswordField, StringField
+from markupsafe import Markup
+from wtforms.fields import (BooleanField, Field, PasswordField, StringField,
+                            SubmitField)
 from wtforms.fields.html5 import EmailField
 from wtforms.validators import DataRequired, Email, Length
+from wtforms.widgets.core import HTMLString, escape, html_params
+
+
+class InlineButtonWidget(object):
+    def __init__(self, class_=None):
+        self.class_ = class_
+
+    def __call__(self, field, **kwargs):
+        kwargs.setdefault('type', 'submit')
+        kwargs["class"] = self.class_
+        title = kwargs.pop('title', field.description or '')
+        params = html_params(title=title, **kwargs)
+
+        html = '<button %s>%s</button>'
+        return HTMLString(html % (params, escape(field.label.text)))
+
+
+class InlineButton(Field):
+    widget = InlineButtonWidget()
+
+    def __init__(self, label=None, validators=None, text='Save', **kwargs):
+        super(InlineButton, self).__init__(label, validators, **kwargs)
+        self.text = text
+
+    def _value(self):
+        if self.data:
+            return u''.join(self.data)
+        else:
+            return u''
 
 
 class LoginForm(FlaskForm):
@@ -27,3 +58,11 @@ class RegisterForm(FlaskForm):
         DataRequired(),
         Length(min=4, message="At least 4 characters are required!")
     ])
+
+
+class BookForm(FlaskForm):
+    name = StringField("Name", validators=[
+        DataRequired()
+    ])
+    text = Markup('<i class="fas fa-sign-in-alt"></i> Submit')
+    submit = SubmitField(text, widget=InlineButtonWidget(class_="btn btn-info"))
