788916eef906d084bb39a28413c4eef83c70b564
Daniel Bailo <danbailoufms@gmail.com>
2020-06-02 17:47:10 -0300

    checking if components existing

--- src/tests/test.py
+++ src/tests/test.py
@@ -16,6 +16,16 @@ def client():
     context.pop()
 
 
-def test_client(client):
-    request = client.get("/")
-    assert request.status_code == 200
+def test_status_code(client):
+    response = client.get("/")
+    assert response.status_code == 200
+
+
+def test_check_register(client):
+    response = client.get("/")
+    assert "Register" in response.get_data(as_text=True)
+
+
+def test_check_login(client):
+    response = client.get("/")
+    assert "Login" in response.get_data(as_text=True)
